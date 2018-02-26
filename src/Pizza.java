6f9ffbc7c912cfff9e0d3cd15316131f0314f9db
JoaoFerreira100 <32512196+JoaoFerreira100@users.noreply.github.com>
2018-02-26 20:39:55 +0100

    FUCK YYOU

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Pizza {
+    Scanner scan = new Scanner(System.in);
+    int R;
+    int C;
+    int L;
+    int H;
+    int[][] pizza;
+
+    void readinator() {
+        R = scan.nextInt();
+        C = scan.nextInt();
+        L = scan.nextInt();
+        H = scan.nextInt();
+
+        pizza = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; i < C; i++) {
+                if(scan.next() == "T" {
+
+                }
+            }
+        }
+    }
 }
